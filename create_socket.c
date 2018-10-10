bbc90c240cb0d0851ba27198af35a323a199d385
cvaneycken <colin.vaneycken@student.uclouvain.be>
2018-10-10 18:21:39 +0200

    debuggage

--- create_socket.c
+++ create_socket.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include "create_socket.h"
+#include <errno.h>      /* errno */
 
 /* Creates a socket and initialize it
  * @source_addr: if !NULL, the source address that should be bound to this socket
@@ -28,28 +29,32 @@ int create_socket(struct sockaddr_in6 *source_addr, int src_port, struct sockadd
     //@src_port: if >0, the port on which the socket is listening
     //cast src_port to "u_int16m_t" bc of sin6_port's type:
     /* struct sockaddr_in6 {
-     *    u_char           sin6_len;      //length of this structure
-     *    u_char           sin6_family;   // AF_INET6
-     *    u_int16m_t       sin6_port;     // Transport layer port #
-     *    u_int32m_t       sin6_flowinfo; // IPv6 flow information
-     *    struct in6_addr  sin6_addr;     // IPv6 address
-     * };
+     * sa_family_t     sin6_family;   //
+     * in_port_t       sin6_port;     // port
+     * uint32_t        sin6_flowinfo; // IPv6 flow
+     * struct in6_addr sin6_addr;     // IPv6
+     * uint32_t        sin6_scope_id; // Scope ID (new in 2.4
+     *};
      */
     if(src_port>0){
-      source_addr->sin6_port=(u_int16m_t)src_port;
+      source_addr->sin6_port=htons(src_port);
     }
-    int err=bind(descriptor,(const struct sockaddr *)source_addr,sizeof(source_addr));
-    fprintf(stderr, "sizeof(source_addr):%d sizeof(struct sockaddr_in6):%d\n",(int)sizeof(source_addr),(int)sizeof(struct sockaddr_in6));
+    int err=bind(descriptor,(const struct sockaddr*)source_addr,sizeof(struct sockaddr_in6));
     if(err==-1){
-      fprintf(stderr, "Fail:bind function\n");
+      fprintf(stderr, "Fail:bind function, %s\n",strerror(errno));
       return -1;
     }
   }
   //connecting client
-  err=connect(descriptor,(const struct sockaddr *)dest_addr,sizeof(dest_addr));
-  if(err==-1){
+  if(dest_addr!=NULL){
+    if(dst_port>0){
+      dest_addr->sin6_port=htons(dst_port);
+    }
+  int err2=connect(descriptor,(const struct sockaddr *)dest_addr,sizeof(dest_addr));
+  if(err2==-1){
     fprintf(stderr, "Fail:connect function\n");
     return -1;
   }
+}
   return descriptor;
 }
