43f625697d80571f2a129319f324753e8bdfc2cc
cvaneycken <colin.vaneycken@student.uclouvain.be>
2018-10-15 22:54:39 +0200

    packet_implem.c en dev (arreté à decode crc1)

--- Packet/packet_implem.c
+++ Packet/packet_implem.c
@@ -4,15 +4,18 @@
 #include <string.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <zlib.h>
 /* Extra #includes */
 /* Your code will be inserted here */
 
 struct __attribute__((__packed__)) pkt {
-ptypes_t type;
+//Header begins
+uint8_t type:2;
 uint8_t trFlag:1;
 uint8_t window:5;
 uint8_t seqNum: 8;
 uint16_t length;
+//Header ends -> 32 Bytes
 uint32_t timeStamp;
 uint32_t crc1;
 char *payload;
@@ -20,6 +23,52 @@ uint32_t crc2;
 };
 
 /* Extra code */
+//Displays adequat error
+pkt_status_code dispErr(pkt_status_code a){
+	pkt_status_code c;
+	switch (c=a) {
+		case PKT_OK:
+			return PKT_OK;
+			break;
+		case E_TYPE:
+			fprintf(stderr, "Erreur liee au champs Type\n");
+			return c;
+			break;
+		case E_TR:
+			fprintf(stderr, "Erreur liee au champ TR\n");
+			return c;
+			break;
+		case E_LENGTH:
+			fprintf(stderr, "Erreur liee au champ Length\n");
+			return c;
+			break;
+		case E_CRC:
+			fprintf(stderr, "CRC invalide\n");
+			return c;
+			break;
+		case E_WINDOW:
+			fprintf(stderr, "Erreur liee au champ Window\n");
+			return c;
+			break;
+		case E_SEQNUM:
+			fprintf(stderr, "Numero de sequence invalide\n");
+			return c;
+			break;
+		case E_NOMEM:
+			fprintf(stderr, "Pas assez de memoire\n");
+			return c;
+			break;
+		case E_NOHEADER:
+			fprintf(stderr, "Le paquet n'a pas de header (trop court)\n");
+			return c;
+			break;
+		case E_UNCONSISTENT:
+			fprintf(stderr, "Le paquet est incoherent\n");
+			return c;
+			break;
+	}
+	return PKT_OK;
+}
 /* Your code will be inserted here */
 
 pkt_t* pkt_new()
@@ -34,26 +83,57 @@ void pkt_del(pkt_t *pkt)
 
 pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 {
-  if(memcpy((void *)pkt,(const void *)data,(size_t)len)==NULL)return E_NOMEM;
-  pkt_set_length(pkt,ntohs(pkt_get_length((const pkt_t *)pkt)));
-  pkt_set_crc1(pkt,ntohs(pkt_get_crc1((const pkt_t *)pkt)));
-  pkt_set_crc2(pkt,ntohs(pkt_get_crc2((const pkt_t *)pkt)));
-  return PKT_OK;
+	//header is 32 Bytes long
+	char *header=malloc(sizeof(uint32_t));
+	if(header==NULL) return dispErr(E_NOMEM);
+	if(len<sizeof(uint32_t)) return dispErr(E_NOHEADER);
+	memcpy((void *)header,(const void *)data,sizeof(uint32_t));
+	//Writing TYPE which is defined by 2 1st bits
+	const ptypes_t type=(*header)>>6;
+	pkt_set_type(pkt,type);
+	//Writing the Tr bit
+	uint8_t h=((*header)>>5)&0b00000001;
+	pkt_set_tr(pkt,h);
+	//Writing  the window
+	h=(*header)&0b00011111;
+	pkt_set_window(pkt,h);
+	//Writing the seqNum which is from 9th to 19h bit so header+1=After 1 byte
+	h=*(header+1);
+	pkt_set_seqnum(pkt,h);
+	//Writing length
+	uint16_t length;
+	memcpy(&length,(header+2),sizeof(uint16_t));
+	if(length>(uint16_t)MAX_PAYLOAD_SIZE) return dispErr(E_LENGTH);
+	pkt_set_length(pkt,ntohs(length));
+	// Writing timestamp
+	uint32_t ts;
+	if(ts==NULL)return dispErr(NO_MEM);
+	memcpy(&ts,(data+4),sizeof(uint32_t));
+	pkt_set_timestamp(pkt,ts);
+	//Writing CRC1
+	uint32_t crc1;
+	memcpy(&crc1,(data+8),sizeof(uint32_t));
+	crc1=ntohl(crc1);
+	if(crc1!=(uint32_t)crc32(0,(Bytef *)data,(uInt)(2*sizeof(uint32_t)))){
+		fprintf(stderr, "Err: decode, crc1 not matching\n");
+		pkt_del(pkt);
+		return dispErr(E_CRC);
+	}
+	pkt_set_crc1(pkt,crc1);
+	//Writing payload
+	char *payload=malloc(sizeof(length));
+	if(payload==NULL)return dispErr(E_NOMEM);
+	memcpy(payload,(data+4),length);
+	pkt_set_payload(pkt,payload,length);
+	//Writing Crc1
+
+
+	return PKT_OK;
 }
 
 pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 {
-	pkt_set_length((pkt_t*)pkt,htons(pkt_get_length((const pkt_t *)pkt)));
-	pkt_set_crc1((pkt_t *)pkt,htons(pkt_get_crc1((const pkt_t *)pkt)));
-	pkt_set_crc2((pkt_t *)pkt,htons(pkt_get_crc2((const pkt_t *)pkt)));
-  if(memcpy((void *)pkt,(const void*)buf,(size_t)len)==NULL){
-    fprintf(stderr, "Error: pkt_encode fwrite, %s\n",strerror(errno));
-    return E_UNCONSISTENT;
-  }
-	pkt_set_length((pkt_t *)pkt,ntohs(pkt_get_length((const pkt_t *)pkt)));
-  pkt_set_crc1((pkt_t *)pkt,ntohs(pkt_get_crc1((const pkt_t *)pkt)));
-  pkt_set_crc2((pkt_t *)pkt,ntohs(pkt_get_crc2((const pkt_t *)pkt)));
-  return PKT_OK;
+	return PKT_OK;
 }
 
 ptypes_t pkt_get_type(const pkt_t* pkt)
--- Packet/testnul.c
+++ Packet/testnul.c
@@ -5,13 +5,57 @@
 #include <errno.h>
 #include <limits.h>
 #include <sys/ioctl.h>
+#include <inttypes.h>
 
-int main(int argc, char const *argv[]) {
-  char buf[1024];
-  while(1){
-    scanf("%1023s", buf);
-    if(buf!=NULL){
-      printf("Il y a qqchose: %s\n", buf);
+
+int check(uint32_t data){
+  uint8_t header=(uint8_t)(data>>24);
+  uint16_t length=(uint16_t)(data);
+
+  printf("Length of: %d\n", length);
+  //uint8_t tr_test=0b00000001;
+  //printf("%d\n",header&tr_test);
+  //uint8_t win_test=0b00011111;
+
+uint8_t headertype=header>>6;
+uint8_t headertr=(header&0b00100000)>>5;
+
+uint8_t headerwin=header&0b00011111;;
+if(headertype==(uint8_t)1){
+  printf("It's a DATA %d\n", headertype);
+}
+
+if(headertype==(uint8_t)2){
+  printf("It's an ACK %d\n", headertype);
+}
+
+if(headertype==(uint8_t)3){
+  printf("It's a NACK %d\n", headertype);
+}
+if(headertype!=(uint8_t)1&&headertype!=(uint8_t)2&&headertype!=(uint8_t)3){
+  printf("TYPE unknown\n");
+}
+if(headertr==1){
+  printf("Truncated\n");
+}
+else{
+  printf("Not truncated %d\n",headertr);
+}
+printf("Window of size:%d\n", headerwin);
+return 0;
+}
+int main(int argc, char const *argv[]){
+    //uint8_t taille=(uint8_t)15;
+    //uint8_t header=0b10100000;
+    //header=header|taille;
+    uint32_t data[6]={0b01101101000001110000010100111111,0b11011001000010100000000000000000,0b11010010101010110000000101011111,0b11011101010111110000000000000000,0b0001111111110000000010111010,0b11010011011111000000000100100000};
+    for(int i=0;i<6;i++){
+      printf("Number %d\n",i);
+      check(data[i]);
     }
-  }
+    printf("%d\n",(int)sizeof(uint32_t));
+    char *truc=malloc(sizeof(char)*3);
+    truc="okla";
+    printf("%c\n",*(truc+1));
+return 0;
 }
